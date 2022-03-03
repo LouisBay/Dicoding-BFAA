fce9931cb69c3160a8bb6d37887466aac5ca0682
Louis Bayu Krisna Redionando <louisbayu21@gmail.com>
2022-03-03 19:39:40 +0700

    Exercise 2 : TabLayout with ViewPager2
    Change to one fragment only (Fragment Home) for all tab menu.
    Send data with bundle to change content for every tab.

--- MyTabLayout/app/src/main/java/com/louis/mytablayout/SectionsPagerAdapter.kt
+++ MyTabLayout/app/src/main/java/com/louis/mytablayout/SectionsPagerAdapter.kt
@@ -1,22 +1,27 @@
 package com.louis.mytablayout
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
 
+    var appName: String = ""
+
     override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
+        var fragment = HomeFragment()
+
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+            putString(HomeFragment.ARG_NAME, appName)
         }
-        return fragment as Fragment
+
+        return fragment
     }
 
     override fun getItemCount(): Int {
-        return 2
+        return 3
     }
 
 }
