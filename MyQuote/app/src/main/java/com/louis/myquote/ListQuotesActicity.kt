16a6f8609f2e2ed73b76969ffc147dab2b9c14cc
Louis Bayu Krisna Redionando <louisbayu21@gmail.com>
2022-03-04 20:13:21 +0700

    Networking Exercise 2 : Parsing Quote List API Dicoding
    Add onclick function on button at main activity that intent to list quote activity. List Quote Activity contain recycler that show quote list from API.

--- MyQuote/app/src/main/java/com/louis/myquote/ListQuotesActicity.kt
+++ MyQuote/app/src/main/java/com/louis/myquote/ListQuotesActicity.kt
@@ -1,57 +1,65 @@
 package com.louis.myquote
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
-import com.louis.myquote.databinding.ActivityMainBinding
+import com.louis.myquote.databinding.ActivityListQuotesActicityBinding
 import cz.msebera.android.httpclient.Header
-import org.json.JSONObject
+import org.json.JSONArray
 import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
+class ListQuotesActicity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityListQuotesActicityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityListQuotesActicityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        getRandomQuote()
+        supportActionBar?.title = "List of Quotes"
 
-        binding.btnAllQuotes.setOnClickListener {
-            startActivity(Intent(this@MainActivity, ListQuotesActicity::class.java))
-        }
+        val layoutManager = LinearLayoutManager(this)
+        binding.listQuotes.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.listQuotes.addItemDecoration(itemDecoration)
+
+        getListQuotes()
     }
 
-    private fun getRandomQuote() {
+    private fun getListQuotes() {
         binding.progressBar.visibility = View.VISIBLE
         val client = AsyncHttpClient()
-        val url = "https://quote-api.dicoding.dev/random"
+        val url = "https://quote-api.dicoding.dev/list"
         client.get(url, object : AsyncHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Array<Header>, responseBody: ByteArray) {
                 // Jika koneksi berhasil
                 binding.progressBar.visibility = View.INVISIBLE
 
+                val listQuote = ArrayList<String>()
+
                 val result = String(responseBody)
                 Log.d(TAG, result)
                 try {
-                    val responseObject = JSONObject(result)
+                    val jsonArray = JSONArray(result)
 
-                    val quote = responseObject.getString("en")
-                    val author = responseObject.getString("author")
-
-                    with(binding) {
-                        tvQuote.text = quote
-                        tvAuthor.text = author
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        val quote = jsonObject.getString("en")
+                        val author = jsonObject.getString("author")
+                        listQuote.add("\n$quote\n - $author\n")
                     }
+
+                    val adapter = QuoteAdapter(listQuote)
+                    binding.listQuotes.adapter = adapter
                 } catch (e: Exception) {
-                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@ListQuotesActicity, e.message, Toast.LENGTH_SHORT).show()
                     e.printStackTrace()
                 }
             }
@@ -59,20 +67,19 @@ class MainActivity : AppCompatActivity() {
             override fun onFailure(statusCode: Int, headers: Array<Header>, responseBody: ByteArray, error: Throwable) {
                 // Jika koneksi gagal
                 binding.progressBar.visibility = View.INVISIBLE
-
                 val errorMessage = when(statusCode) {
                     401 -> "$statusCode : Bad Request"
                     403 -> "$statusCode : Forbidden"
                     404 -> "$statusCode : Not Found"
                     else -> "$statusCode : ${error.message}"
                 }
-                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@ListQuotesActicity, errorMessage, Toast.LENGTH_SHORT).show()
             }
 
         })
     }
 
     companion object {
-        private val TAG = MainActivity::class.java.simpleName
+        private val TAG = ListQuotesActicity::class.java.simpleName
     }
 }
