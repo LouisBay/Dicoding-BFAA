be6992957f5828a34c4670b78dd210bfd1f54219
Louis Bayu Krisna Redionando <louisbayu21@gmail.com>
2022-03-16 11:50:18 +0700

    File Storage Exercise : Read and Write File (Notepad App)
    Project Name : MyReadWriteFile
    
    What's done :
    - Create a FileModel class to hold user data.
    - Create a FileHelper class to set the file storage configuration.
    - Add ViewBinding to build.gradle
    - Add code for onclick button (new file, open file, save  file) on MainActivity.kt

---
+++ Local Data Persistent/MyReadWriteFile/app/src/main/java/com/louis/myreadwritefile/FileModel.kt
@@ -0,0 +1,6 @@
+package com.louis.myreadwritefile
+
+data class FileModel (
+    var filename: String? = null,
+    var data: String? = null
+)
--- Local Data Persistent/MyReadWriteFile/app/src/main/java/com/louis/myreadwritefile/MainActivity.kt
+++ Local Data Persistent/MyReadWriteFile/app/src/main/java/com/louis/myreadwritefile/MainActivity.kt
@@ -2,10 +2,78 @@ package com.louis.myreadwritefile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.louis.myreadwritefile.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            buttonNew.setOnClickListener(this@MainActivity)
+            buttonOpen.setOnClickListener(this@MainActivity)
+            buttonSave.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_new -> newFile()
+            R.id.button_open -> showList()
+            R.id.button_save -> saveFile()
+        }
+    }
+
+    private fun newFile() {
+        with(binding) {
+            editTitle.setText("")
+            editFile.setText("")
+        }
+        Toast.makeText(this, "Clearing file", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showList() {
+        val items = fileList()
+        val builder = AlertDialog.Builder(this).apply {
+            setTitle("Pilih file yang diinginkan")
+            setItems(items) { dialog, item -> loadData(items[item].toString())}
+        }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    private fun loadData(title: String) {
+        val fileModel = FileHelper.readFromFile(this, title)
+        binding.apply {
+            editTitle.setText(fileModel.filename)
+            editFile.setText(fileModel.data)
+        }
+        Toast.makeText(this, "Loading " + fileModel.filename + " data", Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun saveFile() {
+        when {
+            binding.editTitle.text.toString().isEmpty() -> Toast.makeText(this, "Title harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            binding.editFile.text.toString().isEmpty() -> Toast.makeText(this, "Konten harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            else -> {
+                val title = binding.editTitle.text.toString()
+                val text = binding.editFile.text.toString()
+                val fileModel = FileModel().apply {
+                    filename = title
+                    data = text
+                }
+                FileHelper.writeToFile(fileModel, this)
+                Toast.makeText(this, "Saving " + fileModel.filename + " file", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
